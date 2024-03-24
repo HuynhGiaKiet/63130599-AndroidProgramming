9be9f496c33551a0aff219de2480c7e2a105c9b3
HuynhGiaKiet <kiethuynh1412@gmail.com>
2024-03-24 14:49:31 +0700

    updateCau2

---
+++ Cau2_StudentManagement/app/src/main/java/ntu_63130599/cau2_studentmanagement/Adapter/AdapterSinhVien.java
@@ -0,0 +1,75 @@
+package ntu_63130599.cau2_studentmanagement.Adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import ntu_63130599.cau2_studentmanagement.MainActivity;
+import ntu_63130599.cau2_studentmanagement.R;
+import ntu_63130599.cau2_studentmanagement.model.SinhVien;
+
+public class AdapterSinhVien extends ArrayAdapter<SinhVien> {
+    //Màn hình sử dụng layout này
+    Activity context;
+    //Layout cho từng dòng muốn hiển thị Item
+    int resource;
+    //Danh sách dữ liệu muốn hiển thị lên màn hình
+    @NonNull
+    List<SinhVien> objects;
+
+
+    public AdapterSinhVien(@NonNull Activity context, int resource, @NonNull List<SinhVien> objects) {
+        super(context, resource, objects);
+        this.objects = objects;
+        this.context = context;
+        this.resource = resource;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //Đây là 1 lớp để build layout bình thường thành code java mà android sử dụng dc
+        //load file xml vào hệ thống và build thành code để chương trình có thể sử dụng
+        LayoutInflater layoutInflater = this.context.getLayoutInflater();
+        //Đổi số this.resouce chính là item.xml, ta truyen vào khi gọi adapterSinhVien
+        View row = layoutInflater.inflate(this.resource, null);
+
+        ImageView img= row.findViewById(R.id.imageView);
+        TextView profile = row.findViewById(R.id.profile);
+        CheckBox checkBox = row.findViewById(R.id.checkBox);
+
+        SinhVien sinhVien = this.objects.get(position);
+        if (sinhVien.getGioiTinh()==true)
+        {
+            img.setImageResource(R.drawable.male);
+        }
+        else{
+            img.setImageResource(R.drawable.female);
+        }
+        profile.setText(sinhVien.getID() +"  -  "+sinhVien.getName());
+        //Khi nhấn checkbox sẽ lưu vị trí sangarray vitri bên class main
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (checkBox.isChecked()){
+                    //Gọi arraylisst
+                    //position là vị trí item (dòng thông tin)
+                    MainActivity.vitri.add(position);
+                }
+            }
+        });
+        return row;
+    }
+}
---
+++ Cau2_StudentManagement/app/src/main/java/ntu_63130599/cau2_studentmanagement/MainActivity.java
@@ -0,0 +1,99 @@
+package ntu_63130599.cau2_studentmanagement;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+import ntu_63130599.cau2_studentmanagement.Adapter.AdapterSinhVien;
+import ntu_63130599.cau2_studentmanagement.model.SinhVien;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv_HienThiDS;
+    ArrayList<SinhVien> dsSinhVien = new ArrayList<>();
+    AdapterSinhVien adapterSinhVien;
+
+    //Tạo 1 vị trí click checkbox để xóa nhân viên
+    //phải public và do ở hàm main nên phải dùng static
+    public static  ArrayList<Integer> vitri = new ArrayList<>();
+    Button btnXoa,btnThem;
+    EditText txt_Maso,txt_Ten,txt_Lop;
+    RadioButton btnNam, btnNu;
+    boolean gioiTinh = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        addControls();
+        addEvent();
+    }
+    private void addControls(){
+        btnXoa = findViewById(R.id.btnXoa);
+        btnThem = findViewById(R.id.btnThem);
+        txt_Maso = findViewById(R.id.txt_Maso);
+        txt_Ten = findViewById(R.id.txt_Ten);
+        txt_Lop = findViewById(R.id.txt_Lop);
+        btnNam = findViewById(R.id.btnNam);
+        btnNu = findViewById(R.id.btnNu);
+
+        lv_HienThiDS = findViewById(R.id.dsHienThi);
+        dsSinhVien.add(new SinhVien("NV1","Gia Kiệt","63CNTT2",true));
+        dsSinhVien.add(new SinhVien("NV2","Gia Huy","63CNTT2",true));
+        dsSinhVien.add(new SinhVien("NV3","Gia Long","63CNTT2",true));
+        dsSinhVien.add(new SinhVien("NV4","Gia Bảo","63CNTT2",true));
+        dsSinhVien.add(new SinhVien("NV5","Gia Hân","63CNTT2",false));
+        adapterSinhVien = new AdapterSinhVien(MainActivity.this,R.layout.item_view,dsSinhVien);
+        lv_HienThiDS.setAdapter(adapterSinhVien);
+    }
+    private void addEvent(){
+        //sự kiện xóa
+        btnXoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!vitri.isEmpty())
+                {
+                    for(int k:vitri)
+                    {
+                        dsSinhVien.remove(k);
+                    }
+                    vitri.clear();
+                    adapterSinhVien.notifyDataSetChanged();
+                }else{
+                    Toast.makeText(MainActivity.this,"Chưa chọn sinh viên xóa.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        if (btnNam.isChecked()){
+            gioiTinh = true;
+        }else{
+            gioiTinh = false ;
+        }
+        //Sự kiện thêm
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dsSinhVien.add(new SinhVien(txt_Maso.getText().toString(),txt_Ten.getText().toString(),txt_Lop.getText().toString(),gioiTinh));
+            }
+        });
+    }
+
+
+}
